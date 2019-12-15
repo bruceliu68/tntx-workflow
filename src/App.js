31f8698bb2ad75b58a9bfd8608b1252281940457
bo.liu <bo.liu@tongdun.cn>
2019-12-15 20:42:32 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./index.less";
+import React from "react";
+import { init } from "@rematch/core";
+import * as models from "./models";
+import { Route, Router, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createBrowserHistory } from "history";
+import { routes } from "./router";
+import Error from "@/pages/Error";
+
+const store = init({
+	models
+});
+
+const history = createBrowserHistory();
+
+const { dispatch } = store;
+window.dispatch = dispatch;
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<Provider store={store}>
+				<Router history={history}>
+					<Switch>
+						{
+							routes.map(route => (
+								<Route key={route.path} path={route.path} component={route.component} exact={route.exact} />
+							))
+						}
+						<Route component={Error} />
+					</Switch>
+				</Router>
+			</Provider>
+		);
+	}
+}
+
+export default App;
